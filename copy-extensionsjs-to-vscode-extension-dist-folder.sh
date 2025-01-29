91158183de0790b51191c034ae82f552b2f9e311
Sven Seyfert <sven.seyfert@big-direkt.de>
2025-01-29 21:11:53 +0100

    Added: New package.json script to build and replace the current VSCode extension main dist file.

---
+++ copy-extensionsjs-to-vscode-extension-dist-folder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+version="1.0.13"
+username=$(whoami)
+vscodeExtensionFolder="C:/Users/$username/.vscode/extensions/damien.autoit-$version"
+targetFile="$vscodeExtensionFolder/dist/extension.js"
+newTargetFile="./dist/extension.js"
+
+if [ -f "$targetFile" ]; then
+    mv "$targetFile" "$targetFile.old"
+else
+    echo "Error: Target file '$targetFile' does not exist."
+    exit 1
+fi
+
+if [ -f "$newTargetFile" ]; then
+    cp "$newTargetFile" "$targetFile"
+else
+    echo "Error: New target file '$newTargetFile' does not exist."
+    exit 1
+fi
+
+echo -e "\ncopy-extensionsjs-to-vscode-extension-dist-folder.sh done"
