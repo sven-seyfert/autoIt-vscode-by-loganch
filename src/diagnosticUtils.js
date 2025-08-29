159073ed0cc43be00b9593ce2c85ef264fe351c3
Sven Seyfert <sven.seyfert@big-direkt.de>
2025-08-29 22:21:57 +0200

    Changed: My specific changes to get a extended stable version (v1.0.14).

--- src/diagnosticUtils.js
+++ src/diagnosticUtils.js
@@ -62,18 +62,11 @@ export const parseAu3CheckOutput = (output, collection, documentURI) => {
 
     const diagnosticToAdd = new Diagnostic(diagnosticRange, description, diagnosticSeverity);
 
-    // The determined file path (scriptPath) from the AU3Check output will
-    // not be used, because AU3Check returns a wrong encoded string in case
-    // of special characters like german umlauts (ä, ü, ö) or spanish chars
-    // like "ñ" etc. This behavior will not be fixed soon for AU3Check, so
-    // the documentURI path is used instead, which is correct.
-    const file = documentURI.path.replace(/^\/([a-z]):/, '$1:').replace(/\//g, '\\');
-
-    updateDiagnostics(diagnostics, file, diagnosticToAdd);
+    updateDiagnostics(diagnostics, scriptPath, diagnosticToAdd);
   });
 
-  diagnostics.forEach((diagnosticArray, file) => {
-    collection.set(Uri.file(file), diagnosticArray);
+  diagnostics.forEach((diagnosticArray, scriptPath) => {
+    collection.set(Uri.file(scriptPath), diagnosticArray);
   });
 };
 
